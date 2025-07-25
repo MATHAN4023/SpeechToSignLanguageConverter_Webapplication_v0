61e77ad067cb7ec5caae64ba3bd8c60b44556d4e
Mathan@512001 <116017480+MATHAN4023@users.noreply.github.com>
2025-07-25 16:21:01 +0530

    Update Procfile to set NLTK_DATA environment variable and remove redundant NLTK data path handling from views

--- Server/A2SL/views.py
+++ Server/A2SL/views.py
@@ -18,11 +18,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.tokens import AccessToken
-import os
-import nltk
-nltk_data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../nltk_data"))
-print("Using NLTK data path:", nltk_data_dir)
-nltk.data.path.append(nltk_data_dir)
 
 logger = logging.getLogger(__name__)
 
